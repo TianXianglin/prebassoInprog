a11d11dc3adac9e6bac89cc83c90691e61755b35
Minunno <minunno@ad.helsinki.fi>
2018-04-23 18:01:35 +0100

    Process Yasso weather inputs

--- R/utilStaff.r
+++ R/utilStaff.r
@@ -5,3 +5,22 @@
               'Wstem','W_croot','wf_STKG', 'wf_treeKG','B_tree','Light',"Vharvested","Vtot","soilC",
               "aSW","summerSW","Vmort","gross growth", "GPPspecies","Rh species", "NEP sp"))
 }
+
+
+  aTmean <- function(TAir,nYears){
+    Tmean = colMeans(matrix(TAir,365,nYears))
+    return(Tmean)
+  }
+
+  aTampl <- function(TAir,nYears){
+    monthsDays <- c(rep(1,31),rep(2,28),rep(3,31),rep(4,30),rep(5,31),rep(6,30),
+                    rep(7,31),rep(8,31),rep(9,30),rep(10,31),rep(11,30),rep(12,31))
+    TbyYear <- matrix(TAir,365,nYears)
+    Tampl = apply(TbyYear, 2, function(x) max(aggregate(x,by=list(monthsDays),FUN=mean)) - min(aggregate(x,by=list(monthsDays),FUN=mean))  )
+    return(Tampl)
+  }
+
+  aPrecip <- function(Precip,nYears){
+    aP = colSums(matrix(Precip,365,nYears))
+    return(aP)
+  }
