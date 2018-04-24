4c592c21460b9e421d3488936f8ac37226440d15
Minunno <minunno@ad.helsinki.fi>
2018-04-24 15:38:18 +0100

    test

--- src/B_prebas_v0.f90
+++ src/B_prebas_v0.f90
@@ -980,9 +980,9 @@ modOut((year+1),7:nVar,:,:) = outt(7:nVar,:,:)
    litterSize(2,ijj) = 2.
    litterSize(3,ijj) = 0.
    
-   call compAWENH(Lf(ijj),folAWENH(ijj,:),pAWEN(1:4,int(outt(4,ijj,1))))   !!!awen partitioning foliage
-   call compAWENH(Lb(ijj),fbAWENH(ijj,:),pAWEN(5:8,int(outt(4,ijj,1))))   !!!awen partitioning branches
-   call compAWENH(Lst(ijj),stAWENH(ijj,:),pAWEN(9:12,int(outt(4,ijj,1))))         !!!awen partitioning stems
+   ! call compAWENH(Lf(ijj),folAWENH(ijj,:),pAWEN(1:4,int(outt(4,ijj,1))))   !!!awen partitioning foliage
+   ! call compAWENH(Lb(ijj),fbAWENH(ijj,:),pAWEN(5:8,int(outt(4,ijj,1))))   !!!awen partitioning branches
+   ! call compAWENH(Lst(ijj),stAWENH(ijj,:),pAWEN(9:12,int(outt(4,ijj,1))))         !!!awen partitioning stems
 
    call mod5c(pYasso,t,weatherYasso(year,:),soilC((year),:,1,ijj),stAWENH(ijj,:),litterSize(1,ijj), &
 	leac,soilC((year+1),:,1,ijj),steadystate_pred)
