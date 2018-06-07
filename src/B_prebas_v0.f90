b6d3481e9adbfc80df654c072fed606e3e81d7cd
checcomi <checcomi@gmail.com>
2018-06-07 21:03:34 +0100

    test

--- src/B_prebas_v0.f90
+++ src/B_prebas_v0.f90
@@ -314,11 +314,10 @@ end do !!!!!!!end loop layers
       
 if (year <= maxYearSite) then
 	nSpec = nSp
-   ! call Ffotos2(STAND_all,nLayers,nSpec,pCrobas,&
-		! nVar,nPar,MeanLight,coeff,fAPARsite)
-   STAND_all(36,:) = 0.5!MeanLight
-   STAND_all(23,:) = 0.5!coeff
-   fAPARsite=0.5
+   call Ffotos2(STAND_all,nLayers,nSpec,pCrobas,&
+		nVar,nPar,MeanLight,coeff,fAPARsite)
+   STAND_all(36,:) = MeanLight
+   STAND_all(23,:) = coeff
 
    if(fAPARsite == 0. .and. yearX == 0) then
 	if((nYears-year)<10) then
