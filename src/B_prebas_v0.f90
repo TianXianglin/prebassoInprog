fec68e2d3e58a8e658959b38060f63661034396d
checcomi <checcomi@gmail.com>
2018-06-07 15:51:58 +0100

    test

--- src/B_prebas_v0.f90
+++ src/B_prebas_v0.f90
@@ -313,8 +313,8 @@ end do !!!!!!!end loop layers
 !do siteNo = 1, nSites
       
 if (year <= maxYearSite) then
-   call Ffotos2(STAND_all,nLayers,nSp,pCrobas,&
-		nVar,nPar,MeanLight,coeff,fAPARsite)
+   ! call Ffotos2(STAND_all,nLayers,nSp,pCrobas,&
+		! nVar,nPar,MeanLight,coeff,fAPARsite)
    STAND_all(36,:) = 0.5!MeanLight
    STAND_all(23,:) = 0.5!coeff
 
