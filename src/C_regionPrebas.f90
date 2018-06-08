498f16040df7bc61ba711ba0d647423e9d572bce
checcomi <checcomi@gmail.com>
2018-06-08 22:24:15 +0100

    test

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -119,7 +119,7 @@ do ij = 1,maxYears
 
 	if(prebasVersion(i)==0.) then
 
-	   xx = 1!int(min(siteInfo(i,3),5.))
+	   xx = int(siteInfo(i,3))
 	   pCrobasX = pCrobasST(:,:,xx)
 
 	  call prebas_v0(1,nLayers(i),allSP,siteInfo(i,:),pCrobasX,initVar(i,:,1:nLayers(i)),&
