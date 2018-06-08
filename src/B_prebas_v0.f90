6f1308ece5afca060a1d804f47264cb86a161f0a
checcomi <checcomi@gmail.com>
2018-06-08 13:56:09 +0100

    test

--- src/B_prebas_v0.f90
+++ src/B_prebas_v0.f90
@@ -317,11 +317,10 @@ if (year <= maxYearSite) then
 	ll = nLayers
     ! call Ffotos2(STAND_all,nLayers,nSpec,pCrobas,&
 		! nVar,nPar,MeanLight,coeff,fAPARsite)
-	call Ffotos2(MeanLight,coeff,fAPARsite)
-   
-   STAND_all(36,:) = MeanLight(1:nLayers)
-   STAND_all(23,:) = coeff(1:nLayers)
-! fAPARsite=0.7
+	! call Ffotos2(MeanLight,coeff,fAPARsite)
+   STAND_all(36,:) = 0.5!MeanLight(1:nLayers)
+   STAND_all(23,:) = 0.3!coeff(1:nLayers)
+ fAPARsite=0.7
    if(fAPARsite == 0. .and. yearX == 0) then
 	if((nYears-year)<10) then
 	 Ainit = nint(6. + 2*3.5 - 0.005*modOut(year,5,1,1) + 2.25)
