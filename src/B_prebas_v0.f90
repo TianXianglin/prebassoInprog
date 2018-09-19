7d762ef54def28ecd1583b46e57d399436030673
checcomi <checcomi@gmail.com>
2018-09-19 17:00:51 +0100

    test

--- src/B_prebas_v0.f90
+++ src/B_prebas_v0.f90
@@ -538,9 +538,9 @@ endif
 
 	  !!!calculate deadWood using Gompetz function (Makinen et al. 2006)!!!!
 	  if(dN<0.) then
-	  modOut((year+1),8,ij,1) = modOut((year+1),8,ij,1) + Vold* min(1.,-dN*step/Nold)
+	  ! modOut((year+1),8,ij,1) = modOut((year+1),8,ij,1) + Vold* min(1.,-dN*step/Nold)
 	    do ijj = 1,(nyears-year)
-			modOut((year+ijj+1),8,ij,1) = modOut((year+ijj+1),8,ij,1) + 100+(Vold/Nold) * (-dN*step) * &
+			modOut((year+ijj+1),8,ij,1) = modOut((year+ijj+1),8,ij,1) + (Vold/Nold) * (-dN*step) * &
 				exp(-exp(pCrobas(34,species) + pCrobas(35,species)*ijj + pCrobas(36,species)*D + 0.))
 		enddo
 	  end if
