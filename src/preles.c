84f4db49d5e47f103b52e4f33f216e306f67eeae
checcomi <checcomi@gmail.com>
2018-10-22 13:58:49 +0100

    test

--- src/preles.c
+++ src/preles.c
@@ -70,7 +70,7 @@ int preles(int NofDays,
   flog = fopen("preles.log", "a"); // EXCEPTION LOGGING
   LOGFLAG = 1;
   
-  if (LOGFLAG > 0.5) fprintf(flog, "  Stepped into preles()");
+  //if (LOGFLAG > 0.5) fprintf(flog, "  Stepped into preles()");
   
   double I, T, D, P, theta, theta_snow, theta_canopy, S_state;
   double PhenoS=0;
@@ -87,13 +87,13 @@ int preles(int NofDays,
   S_state = S[0];
   
   
-  if (LOGFLAG > 1.5) {
-    fprintf(flog, "   preles(): Starting values for storage components:\nSW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n"
-	    "   ...will loop %d rows of weather input\n",
-	    theta, theta_canopy, theta_snow, S[0], NofDays);
-    printf("   preles(): Site fAPAR =%lf,  LUE = %lf and soil depth = %lf.\n",
-	   fAPAR[0], GPP_par.beta, Site_par.soildepth);
-  }
+  // if (LOGFLAG > 1.5) {
+    // fprintf(flog, "   preles(): Starting values for storage components:\nSW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n"
+	    // "   ...will loop %d rows of weather input\n",
+	    // theta, theta_canopy, theta_snow, S[0], NofDays);
+    // printf("   preles(): Site fAPAR =%lf,  LUE = %lf and soil depth = %lf.\n",
+	   // fAPAR[0], GPP_par.beta, Site_par.soildepth);
+  // }
   //    fclose(flog);
   
 
@@ -106,10 +106,10 @@ int preles(int NofDays,
   /* ---------------- ----------------------------------------------------*/
   for (i=0; i < NofDays; i++) { 
     
-    if ((LOGFLAG > 1.5)) {
-      fprintf(flog, "   \ni=%d/%d,\t SW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n",
-	      i+1, NofDays, theta, theta_canopy, theta_snow, S_state);
-    }      
+    // if ((LOGFLAG > 1.5)) {
+      // fprintf(flog, "   \ni=%d/%d,\t SW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n",
+	      // i+1, NofDays, theta, theta_canopy, theta_snow, S_state);
+    // }      
     
     /* Use previous day environment for prediction, if current values are missing,
        or suspicious*/
@@ -130,10 +130,10 @@ int preles(int NofDays,
     }
     
     
-    if ((LOGFLAG > 1.5)) {
-      fprintf(flog, "   weather inputs: PAR=%lf\tT=%lf\tVPD=%lf\tP=%lf\tCO2=%lf\n",
-	      PAR[i], TAir[i], VPD[i], Precip[i], CO2[i]);
-    }   
+    // if ((LOGFLAG > 1.5)) {
+      // fprintf(flog, "   weather inputs: PAR=%lf\tT=%lf\tVPD=%lf\tP=%lf\tCO2=%lf\n",
+	      // PAR[i], TAir[i], VPD[i], Precip[i], CO2[i]);
+    // }   
     
     /* Assign current values for environment */
     I = PAR[i]; T=TAir[i]; D=VPD[i]; P=Precip[i]; 
@@ -141,35 +141,35 @@ int preles(int NofDays,
      * for GPP and through GPP to ET */
     fS[i] = fS_model(&S_state, T, GPP_par);
 
-    if (LOGFLAG > 1.5) fprintf(flog, "   preles(): estimated fS=%lf\n", fS[i]);
+    // if (LOGFLAG > 1.5) fprintf(flog, "   preles(): estimated fS=%lf\n", fS[i]);
     
     
     /* Deciduous phenology - don't use if this information is inputted in fAPAR */
     /* Note also that fapar is multiplied by 0 or 1 (i.e. leaf development is not accounted for) */
     /* Model predicts budbreak based on critical threshold temperature sum */ 
     /* Note that this implementation works only if data starts before t0-date of fPheno-model */      
-    if (LOGFLAG > 1.5) fprintf(flog, 
-			       "   preles(): stepping into fPheno_model: inputs:\n      GPP_par.t0=%lf\tT=%lf\tPhenoS=%lf\tDOY=%d\n", 
-			       GPP_par.t0, T, PhenoS, day[i]);
+    // if (LOGFLAG > 1.5) fprintf(flog, 
+			       // "   preles(): stepping into fPheno_model: inputs:\n      GPP_par.t0=%lf\tT=%lf\tPhenoS=%lf\tDOY=%d\n", 
+			       // GPP_par.t0, T, PhenoS, day[i]);
 
     fPheno = fPheno_model(GPP_par, T, &PhenoS, day[i], fS[i]);
-    if (LOGFLAG > 1.5) fprintf(flog, "   preles(): PhenoS=%lf\tfPheno=%lf\n", 
-			       PhenoS, fPheno );
+    // if (LOGFLAG > 1.5) fprintf(flog, "   preles(): PhenoS=%lf\tfPheno=%lf\n", 
+			       // PhenoS, fPheno );
 
     fAPAR[i] = fAPAR[i] * fPheno; 
-    if (LOGFLAG > 1.5) fprintf(flog, 
-			       "   preles(): fAPAR changed to %lf\n", 
-			       fAPAR[i]);
+    // if (LOGFLAG > 1.5) fprintf(flog, 
+			       // "   preles(): fAPAR changed to %lf\n", 
+			       // fAPAR[i]);
     
 
     GPPfun(&GPP[i], &gpp380, I, D, CO2[i], theta, fAPAR[i], fS[i],
 		    GPP_par, Site_par,  &fD[i], &fW[i], &fEgpp, 
 		    flog, LOGFLAG );
 
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, 
-	      "   preles(): estimated GPP=%lf\tfD=%lf\tfEgpp=%lf\n GPP380ppm %lf\n", 
-	      GPP[i], fD[i], fEgpp, gpp380);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, 
+	      // "   preles(): estimated GPP=%lf\tfD=%lf\tfEgpp=%lf\n GPP380ppm %lf\n", 
+	      // GPP[i], fD[i], fEgpp, gpp380);
     
     
     /* Calculate amount of snow and snowmelt at the end of the day */
@@ -180,10 +180,10 @@ int preles(int NofDays,
     interceptionfun(&Throughfall[i], &Interception[i], T, 
 		    SnowRain_par, fAPAR[i]);
     
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, 
-	      "   preles(): estimated Thr.fall=%lf\tIntercept.=%lf\tSOG=%lf\tSnowmelt=%lf\n", 
-	      Throughfall[i], Interception[i], SOG[i], Snowmelt[i]);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, 
+	      // "   preles(): estimated Thr.fall=%lf\tIntercept.=%lf\tSOG=%lf\tSnowmelt=%lf\n", 
+	      // Throughfall[i], Interception[i], SOG[i], Snowmelt[i]);
     
     /*Excess water from canopy will drip down to soil if not evaporated 
       during the day, rest remains in canopy for the next day*/
@@ -200,9 +200,9 @@ int preles(int NofDays,
       }     
     }
     
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, "   preles(): estimated canopy water=%lf\n", 
-	      theta_canopy);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, "   preles(): estimated canopy water=%lf\n", 
+	      // theta_canopy);
     
     
     ET[i] = ETfun(D, theta, I, fAPAR[i], T, 
@@ -217,9 +217,9 @@ int preles(int NofDays,
 		  &transp[i], 
 		  &evap[i], &fWE[i]);                     
     
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, 
-	      "   preles(): ET=%lf\n", ET[i]);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, 
+	      // "   preles(): ET=%lf\n", ET[i]);
 
      
       /* Calculate soil water balance, drainage and related variables at the 
@@ -230,10 +230,10 @@ int preles(int NofDays,
 	      Site_par, &Drainage[i], //&Psi[i], &Ks[i], 
 	      &theta_snow, &theta_canopy, SnowRain_par);
 
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, 
-	      "   preles(): drainage=%lf, after ET: SW=%lf\tSOG=%lf\tCW=%lf\n", 
-	      Drainage[i], theta, theta_snow, theta_canopy);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, 
+	      // "   preles(): drainage=%lf, after ET: SW=%lf\tSOG=%lf\tCW=%lf\n", 
+	      // Drainage[i], theta, theta_snow, theta_canopy);
     
     /* Record result variables with storage components */
     SOG[i] = theta_snow;
@@ -241,9 +241,9 @@ int preles(int NofDays,
     Canopywater[i] = theta_canopy;
     S[i]=S_state;
     
-    if (LOGFLAG > 1.5) 
-      fprintf(flog, 
-	      "   preles(): after day state:\n   SW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n\n", SW[i], Canopywater[i], SOG[i], S[i]);
+    // if (LOGFLAG > 1.5) 
+      // fprintf(flog, 
+	      // "   preles(): after day state:\n   SW=%lf\tCW=%lf\tSOG=%lf\tS=%lf\n\n", SW[i], Canopywater[i], SOG[i], S[i]);
 
     
   } // END DAY LOOP
